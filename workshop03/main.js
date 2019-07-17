fa920fe58d3bb22e41580f315c8b8c88cb6196e6
belleku_ <vanewen_6@hotmail.com>
2019-07-17 16:23:56 +0800

    workshop03 commit

--- workshop03/main.js
+++ workshop03/main.js
@@ -24,8 +24,8 @@ console.info(`Using ${keys.mongo}`);
 // if they are not the defaults below
 const db = CitiesDB({  
 	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+	databaseName: 'cities', 
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -35,13 +35,132 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // TODO 1/2 Load schemans
+//load the schema from directory
+const citySchema = require('./schema/city-schema.json');
 
-
-
+new OpenAPIValidator({ apiSpecPath: join(__dirname, 'schema','city-api.yaml')
+}).install(app)
 
 // Start of workshop
 // TODO 2/2 Copy your routes from workshop02 here
+// Start of workshop
 
+// Mandatory workshop
+// TODO GET /api/states
+
+app.get('/api/states',(req, resp) => 
+{
+  //content type
+  resp.type('application/json')
+  
+  db.findAllStates()
+	.then(result => {
+	  //result code = 200
+	  resp.status(200)
+	  resp.set('X-Date', (new Date()).toUTCString())
+	  resp.json(result);
+	 })
+	.catch(error => {
+	   // error code to return 
+	  resp.status(400)
+	  resp.json({ error: error})
+	 });
+});
+
+
+// TODO GET /api/state/:state
+app.get('/api/state/:state',(req, resp) => 
+{
+const stateAbbrev = req.params.state;
+//content type
+resp.type('application/json')
+db.findAllStates()
+ .then(result => {
+   if (result.indexOf(stateAbbrev.toUpperCase()) < 0)
+   {
+	   resp.status(400);
+	   resp.json({ error: `Not a valid state: ${stateAbbrev}`})
+	   return;
+   }
+   return (db.findCitiesByState(stateAbbrev));
+ })
+	.then(result => {
+	  //result code = 200
+	  resp.status(200)
+	  resp.json(result.map(v => `/api/city/${v}`));
+	 })
+	.catch(error => {
+	   // error code to return 
+	  resp.status(400)
+	  resp.json({ error: error})
+	 });
+
+});
+
+
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:cityId',
+  (req, resp) => {
+const stateAbbrev = req.params.cityId;
+//content type
+resp.type('application/json')
+
+  db.findCityById(stateAbbrev)
+  .then(result => {
+	  //check whether can find the city ID in DB
+	  if(result === undefined || result.length == 0 ) 
+	  {
+	  resp.status(404)
+	  resp.json({ error: `City not found: ${stateAbbrev}`})
+	  return;
+	  }
+	//result code = 200
+	resp.status(200)
+	resp.json(result[0]);
+   })
+  .catch(error => {
+	 // error code to return 
+	resp.status(400)
+	resp.json({ error:  error})
+   });
+
+});
+
+
+// TODO POST /api/city
+// Content-Type: application/json
+/*
+  {
+  "city" : "BARRE",
+  "loc" : [ 
+	  -72.108354, 
+	  42.409698
+  ],
+  "pop" : 4546,
+  "state" : "MA"
+}
+*/
+app.post('/api/city',
+schemaValidator.validate({ body: citySchema }),
+ (req,resp) =>{
+	 const newCity = req.body;
+  //content type 
+  resp.type('application/json')
+  db.insertCity(newCity)
+  .then(
+	   result => {
+		  resp.status(201)
+		  resp.json(result);
+	   }
+  )
+  .catch(error => {
+	  // error code to return 
+	 resp.status(400)
+	 resp.json({ error:  error})
+	});
+ } 
+);
 
 // End of workshop
 
